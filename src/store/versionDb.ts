c8144daeb413b583de98ada2987bff4495a36236
Yashu Mittal <mittalyashu77@gmail.com>
2025-02-19 15:58:18 +0530

    create FlowDB in indexedDb

---
+++ src/store/versionDb.ts
@@ -0,0 +1,24 @@
+import Dexie from "dexie";
+
+const db = new Dexie("FlowDB");
+db.version(1).stores({
+  versions: "++id, name, nodes, edges, created_at"
+});
+
+export const saveVersion = async (name: string, nodes: unknown, edges: unknown) => {
+  // @ts-expect-error - ???
+  await db.versions.add({
+    name, nodes, edges, created_at: new Date().toJSON() });
+};
+
+export const getVersions = async () => {
+  // @ts-expect-error - ???
+  return await db.versions.toArray();
+};
+
+export const getVersionById = async (id: string) => {
+  // @ts-expect-error - ???
+  return await db.versions.get(id);
+};
+
+export default db;
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type IVersion = {
+  id: number;
+  name: string;
+  nodes: unknown;
+  edges: unknown;
+  created_at: Date;
+}
