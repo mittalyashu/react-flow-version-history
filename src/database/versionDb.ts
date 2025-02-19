195338b5f2ba4d42948c1cce81b4fe222c5b3784
Yashu Mittal <mittalyashu77@gmail.com>
2025-02-20 01:27:44 +0530

    format the code

--- src/database/versionDb.ts
+++ src/database/versionDb.ts
@@ -9,21 +9,4 @@ db.version(3).stores({
   versions: "++id, name, nodes, edges, created_at",
 });
 
-export const saveVersion = async (
-  name: string,
-  nodes: unknown,
-  edges: unknown,
-) => {
-  await db.versions.add({
-    name,
-    nodes,
-    edges,
-    created_at: new Date().toJSON(),
-  });
-};
-
-export const getVersionById = async (id: number) => {
-  return await db.versions.get(id);
-};
-
 export default db;
