8a0f6ec1e10d8132781cae0c0a92156af6b8df55
Yashu Mittal <mittalyashu77@gmail.com>
2025-02-20 01:28:10 +0530

    feat: add option to edit label for PositionLoggerNode

--- src/nodes/PositionLoggerNode.tsx
+++ src/nodes/PositionLoggerNode.tsx
@@ -1,12 +1,28 @@
-import { Handle, Position, type NodeProps } from "@xyflow/react";
+import { useState } from "react";
+import { Handle, Position, type NodeProps, useReactFlow } from "@xyflow/react";
+import { PencilIcon } from "lucide-react";
 
-import { type PositionLoggerNode } from "./types";
+import type { PositionLoggerNode as PositionLoggerNodeType } from "./types";
 
 export function PositionLoggerNode({
   positionAbsoluteX,
   positionAbsoluteY,
   data,
-}: NodeProps<PositionLoggerNode>) {
+  id,
+}: NodeProps<PositionLoggerNodeType>) {
+  const reactFlow = useReactFlow();
+  const [label, setLabel] = useState(data.label);
+  const [editing, setIsEditing] = useState(false);
+
+  function updateLabelHandler() {
+    reactFlow.updateNode(id, {
+      data: {
+        label,
+      },
+    });
+    setIsEditing(false);
+  }
+
   const x = `${Math.round(positionAbsoluteX)}px`;
   const y = `${Math.round(positionAbsoluteY)}px`;
 
@@ -17,6 +33,25 @@ export function PositionLoggerNode({
 
       <div>
         {x} {y}
+      </div>
+
+      <div className="flex items-center">
+        <button type="button" onClick={() => setIsEditing(!editing)}>
+          <PencilIcon />
+        </button>
+
+        {editing ? (
+          <div>
+            <input
+              type="text"
+              value={label}
+              onChange={(e) => setLabel(e.target.value)}
+            />
+            <button type="button" onClick={updateLabelHandler}>
+              save
+            </button>
+          </div>
+        ) : null}
       </div>
 
       <Handle type="source" position={Position.Bottom} />
